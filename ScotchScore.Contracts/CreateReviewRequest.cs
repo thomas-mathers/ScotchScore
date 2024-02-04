d12bbab029f77289b1e26ec1c0f058f0cb615dcf
Thomas Mathers <thomas.mathers@ideanotion.net>
2024-02-03 23:47:03 -0500

    changes

--- ScotchScore.Contracts/CreateReviewRequest.cs
+++ ScotchScore.Contracts/CreateReviewRequest.cs
@@ -4,13 +4,19 @@ namespace ScotchScore.Contracts
 {
     public class CreateReviewRequest
     {
-        [Required] public string Description { get; set; } = string.Empty;
+        [Required]
+        public string Description { get; set; } = string.Empty;
 
-        [Required] [Range(0, 5)] public int Rating { get; set; }
+        [Required]
+        [Range(0, 5)]
+        public int Rating { get; set; }
 
-        [Required] public string Title { get; set; } = string.Empty;
+        [Required]
+        public string Title { get; set; } = string.Empty;
 
-        [Required] [EmailAddress] public string UserEmail { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        public string UserEmail { get; set; } = string.Empty;
 
         [Required]
         [MinLength(3)]
--- ScotchScore.Contracts/ScotchSearchParameters.cs
+++ ScotchScore.Contracts/ScotchSearchParameters.cs
@@ -11,7 +11,9 @@ namespace ScotchScore.Contracts
         [DefaultValue(0)]
         public int PageIndex { get; set; }
 
-        [Range(1, 100)] [DefaultValue(100)] public int PageSize { get; set; } = 100;
+        [Range(1, 100)]
+        [DefaultValue(100)]
+        public int PageSize { get; set; } = 100;
 
         public ScotchSortColumn SortBy { get; set; } = ScotchSortColumn.Name;
         public SortDirection SortDirection { get; set; } = SortDirection.Ascending;
