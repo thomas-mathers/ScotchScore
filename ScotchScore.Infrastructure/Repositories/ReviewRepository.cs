280266c3ac083930de1213d80ce8f95233694e8c
Thomas Mathers <thomas.mathers@ideanotion.net>
2024-02-05 23:39:36 -0500

    changes

--- ScotchScore.Infrastructure/Repositories/ReviewRepository.cs
+++ ScotchScore.Infrastructure/Repositories/ReviewRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
 using ScotchScore.Application.Contracts;
 using ScotchScore.Contracts;
 using Review = ScotchScore.Domain.Review;
@@ -7,6 +8,15 @@ namespace ScotchScore.Infrastructure.Repositories;
 
 public class ReviewRepository(DatabaseContext databaseContext) : IReviewRepository
 {
+    private static Dictionary<ReviewSortColumn, Expression<Func<Review, object>>> SortByMapping { get; } =
+        new()
+        {
+            [ReviewSortColumn.DateCreated] = review => review.DateCreated,
+            [ReviewSortColumn.Rating] = review => review.Rating,
+            [ReviewSortColumn.Upvotes] = review => review.Upvotes,
+            [ReviewSortColumn.Downvotes] = review => review.Downvotes
+        };
+    
     public Task<Review?> GetReview(string reviewId, CancellationToken cancellationToken = default)
     {
         return databaseContext.Reviews
@@ -26,9 +36,29 @@ public class ReviewRepository(DatabaseContext databaseContext) : IReviewReposito
         CancellationToken cancellationToken = default
     )
     {
-        var reviews = await databaseContext.Reviews
+        var query = databaseContext.Reviews
             .Where(x => x.ScotchId == scotchId)
-            .OrderBy(x => x.DateCreated)
+            .AsQueryable();
+        
+        if (SortByMapping.TryGetValue(searchParameters.SortBy, out var keySelector))
+        {
+            query = searchParameters.SortDirection == SortDirection.Ascending
+                ? query.OrderBy(keySelector)
+                : query.OrderByDescending(keySelector);
+        }
+        else
+        {
+            query = searchParameters.SortDirection == SortDirection.Ascending
+                ? query.OrderBy(x => x.DateCreated)
+                : query.OrderByDescending(x => x.DateCreated);
+        }
+        
+        if (!string.IsNullOrWhiteSpace(searchParameters.Title))
+        {
+            query = query.Where(x => x.Title.ToLower().Contains(searchParameters.Title.ToLower()));
+        }
+        
+        var reviews = await query
             .Skip(searchParameters.PageIndex * searchParameters.PageSize)
             .Take(searchParameters.PageSize)
             .ToArrayAsync(cancellationToken);
